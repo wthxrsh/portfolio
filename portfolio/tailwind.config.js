800637bb544eb057bc6179c7b3cf150430a65c5d
Harsh <63588817+wthxrsh@users.noreply.github.com>
2024-10-22 20:09:17 +0530

    Add files via upload

---
+++ portfolio/tailwind.config.js
@@ -0,0 +1,11 @@
+tailwind.config = {
+    theme:{
+        gridTemplateColumns:{
+            'auto': 'repeat(auto-fit, minmax(200px, 1fr))'
+        },
+        fontFamily:{
+            Outfit: ["Outfit", "sans-serif"],
+            Ovo: ["Ovo", "serif"]
+        }
+    }
+}
